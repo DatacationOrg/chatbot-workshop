42fda23ec884b0778f3016a4dc0ccd7f97518977
Stijn Hoskens <s.hoskens@datacation.nl>
2025-05-02 15:31:48 +0200

    Make running more foolproof

--- chat_app.py
+++ chat_app.py
@@ -16,3 +16,15 @@ async def on_message(message: cl.Message):
         assert isinstance(chunk_content, str)
         await msg.stream_token(chunk_content)
     await msg.send()
+
+
+def main():
+    from chainlit.cli import run_chainlit
+    from chainlit.config import config as chainlit_config
+
+    chainlit_config.run.watch = True
+    run_chainlit(str(__file__))
+
+
+if __name__ == "__main__":
+    main()
