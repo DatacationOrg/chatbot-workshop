8ec3fb80cd1464cba3576bd257ea1c1fe83b11bf
Stijn Hoskens <s.hoskens@datacation.nl>
2025-05-08 17:09:08 +0200

    Implement part 2 of the workshop

--- chat_app.py
+++ chat_app.py
@@ -1,24 +1,61 @@
 import datetime
+import sqlite3
+from typing import cast
 
 import chainlit as cl
-from langchain_community.tools import DuckDuckGoSearchRun
+import pandas as pd
+import plotly.graph_objects as go
+from datasets import DatasetDict, load_dataset
+from langchain_community.agent_toolkits import SQLDatabaseToolkit
+from langchain_community.tools import DuckDuckGoSearchRun, tool
+from langchain_community.utilities import SQLDatabase
 from langchain_core.messages import AIMessageChunk
 from langchain_core.runnables import RunnableConfig
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langgraph.checkpoint.memory import MemorySaver
+from langgraph.checkpoint.memory import InMemorySaver
 from langgraph.prebuilt import create_react_agent
+from sqlalchemy import create_engine
 
+dataset = cast(DatasetDict, load_dataset("mstz/titanic"))["train"]
+titanic_df = cast(pd.DataFrame, dataset.to_pandas())
+
+
+def _get_engine_for_titanic_db():
+    connection = sqlite3.connect(":memory:", check_same_thread=False)
+    titanic_df.to_sql(
+        "titanic_passengers", connection, if_exists="replace", index=False
+    )
+    return create_engine("sqlite://", creator=lambda: connection)
+
+
+llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash-preview-04-17")
 search_tool = DuckDuckGoSearchRun()
 
+db = SQLDatabase(_get_engine_for_titanic_db())
+data_toolkit = SQLDatabaseToolkit(db=db, llm=llm)
+
+
+show_bar_chart_column = None
+
+
+@tool
+def show_bar_chart(column: str):
+    """Show a bar chart over the specified column."""
+    global show_bar_chart_column
+    show_bar_chart_column = column.lower()
+
+
 agent = create_react_agent(
-    ChatGoogleGenerativeAI(model="gemini-2.5-flash-preview-04-17"),
-    tools=[search_tool],
+    llm,
+    tools=[search_tool, *data_toolkit.get_tools(), show_bar_chart],
     prompt=f"""
-        You are a helpful assistant.
+        You are a helpful exploratory data analysis assistant.
+        You know everything about the Titanic dataset.
+        You can answer questions about the dataset and run SQL queries on it.
         You can answer questions and search the web for information.
         Today is {datetime.date.today()}.
     """,
-    checkpointer=MemorySaver(),
+    checkpointer=InMemorySaver(),
 )
 
 
@@ -47,6 +84,16 @@ async def on_message(message: cl.Message):
                 langgraph_step = current_step
             await final_answer.stream_token(msg.content)
     await final_answer.send()
+    global show_bar_chart_column
+    if show_bar_chart_column is not None:
+        to_plot = titanic_df[show_bar_chart_column]
+        to_plot = to_plot.value_counts(bins=10)
+        fig = go.Figure(data=[go.Bar(x=to_plot.index.astype(str), y=to_plot.values)])
+        await cl.Message(
+            content="",
+            elements=[cl.Plotly(figure=fig, display="inline")],
+        ).send()
+        show_bar_chart_column = None
 
 
 def main():
